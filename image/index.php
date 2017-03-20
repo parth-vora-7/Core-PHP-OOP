c66a646c198958f7ce4f057a90ef302012b92788
Parth vora <parth.vora.777@gmail.com>
2017-03-20 18:52:37 +0530

    Watermark added

--- image/index.php
+++ image/index.php
@@ -5,7 +5,7 @@
 header('Content-Type: image/png');
 
 // The text to draw
-$text = 'Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta.';
+$text = "Of course; there are those critics - New York critics as a rule - who say; 'Well; Maya Angelou has a new book out and of course it's good but then she's a natural writer.' Those are the ones I want to grab by the throat and wrestle to the floor because it";
 
 $text = wordwrap($text, 38, "\n");
 
@@ -15,7 +15,6 @@ $im = imagecreatetruecolor(500, 500);
 // Create some colors
 $white = imagecolorallocate($im, 255, 255, 255);
 $grey = imagecolorallocate($im, 128, 128, 128);
-$black = imagecolorallocate($im, 0, 0, 0);
 imagefilledrectangle($im, 0, 0, 499, 499, $white);
 
 // Replace path by your own font path
@@ -30,7 +29,7 @@ imagettftext($im, 20, 0, 20, 100, $grey, $font, $text);
 // Using imagepng() results in clearer text compared with imagejpeg()
 imagepng($im);
 imagedestroy($im);
-
+exit;
 // */
 
 // To add an watermark image on the image
@@ -39,7 +38,7 @@ $stamp = imagecreatefrompng('stamp.png');
 $im = imagecreatefrompng('download.png');
 
 // Set the margins for the stamp and get the height/width of the stamp image
-$marge_right = 180;
+$marge_right = 30;
 $marge_bottom = 50;
 $sx = imagesx($stamp);
 $sy = imagesy($stamp);
@@ -52,6 +51,3 @@ imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $
 header('Content-type: image/png');
 imagepng($im);
 imagedestroy($im);
-
-
-
